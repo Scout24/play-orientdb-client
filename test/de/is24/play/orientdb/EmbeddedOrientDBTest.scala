788ff31680f5d90fad8571fc031cc0504618edbc
mriehl <max@riehl.io>
2015-09-02 17:01:56 +0200

    add implicit variant to client example

--- test/de/is24/play/orientdb/EmbeddedOrientDBTest.scala
+++ test/de/is24/play/orientdb/EmbeddedOrientDBTest.scala
@@ -3,6 +3,7 @@ package de.is24.play.orientdb
 import de.is24.play.orientdb.testsupport.OrientDBScope
 import org.specs2.mutable.Specification
 import play.api.test.{DefaultAwaitTimeout, FutureAwaits}
+import Operation._
 
 class EmbeddedOrientDBTest extends Specification with FutureAwaits with DefaultAwaitTimeout {
 
@@ -23,6 +24,10 @@ class EmbeddedOrientDBTest extends Specification with FutureAwaits with DefaultA
       val batchOperation = BatchOperation(transaction = true, operations = Seq(CommandOperation(language = "sql", command = "SELECT 1")))
       await(orientClient.executeBatch(batchOperation))
 
+      implicit val client = orientClient
+      await(Seq("SELECT 1", "SELECT 2").transactionally.execute)
+
+      await("INSERT INTO V SET notice = 'inserted transactionally'".transactionally.execute)
     }
   }
 
