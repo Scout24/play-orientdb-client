5b733d3e4f5987f088bd64b037d2f1f08eba6d0f
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-01-28 10:30:52 +0100

    prepare sonatype release

--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,9 @@
-import sbtrelease.ReleasePlugin._
+import sbtrelease.ReleasePlugin.autoImport._
 import sbt.Keys._
 import sbt._
+
+import com.typesafe.sbt.SbtPgp.autoImportImpl._
+import sbtrelease.ReleaseStateTransformations._
 
 object Build extends Build with Dependencies {
 
@@ -9,30 +12,75 @@ object Build extends Build with Dependencies {
 
   lazy val compileScalastyle = taskKey[Unit]("compileScalastyle")
 
-  lazy val root = Project(id = "root", base = file("."), settings = Defaults.coreDefaultSettings ++ releaseSettings ++ net.virtualvoid.sbt.graph.Plugin.graphSettings).settings(
-    name := "play-orientdb-client",
-    organization := "de.is24",
-    scalaVersion := "2.11.7",
-    ivyScala := ivyScala.value map { _.copy(overrideScalaVersion = true) },
-    libraryDependencies ++= appDependencies ++ testDependencies,
-    javacOptions ++= Seq("-source", javaVersion, "-target", javaVersion, "-Xlint"),
-    scalacOptions ++= Seq("-feature", "-language:postfixOps", "-target:jvm-" + javaVersion, "-unchecked", "-deprecation", "-encoding", encoding),
-    compileScalastyle := org.scalastyle.sbt.ScalastylePlugin.scalastyle.in(Compile).toTask("").value,
-    (compile in Compile) <<= (compile in Compile) dependsOn compileScalastyle,
-    resolvers ++= Seq(
-      "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
-      "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
-    ), 
-    publishTo := {
-      val nexus = "http://nexus.rz.is/content/repositories/"
-      if (isSnapshot.value)
-        Some("snapshots" at nexus + "snapshots")
-      else
-        Some("releases" at nexus + "releases")
-    }
+  lazy val root = Project(
+    id = "root",
+    base = file("."),
+    settings = Defaults.coreDefaultSettings ++ net.virtualvoid.sbt.graph.Plugin.graphSettings)
+    .settings(
+      name := "play-orientdb-client",
+      organization := "de.is24",
+      scalaVersion := "2.11.7",
+      ivyScala := ivyScala.value map {
+        _.copy(overrideScalaVersion = true)
+      },
+      libraryDependencies ++= appDependencies ++ testDependencies,
+      javacOptions ++= Seq("-source", javaVersion, "-target", javaVersion, "-Xlint"),
+      scalacOptions ++= Seq("-feature", "-language:postfixOps", "-target:jvm-" + javaVersion, "-unchecked", "-deprecation", "-encoding", encoding),
+      compileScalastyle := org.scalastyle.sbt.ScalastylePlugin.scalastyle.in(Compile).toTask("").value,
+      (compile in Compile) <<= (compile in Compile) dependsOn compileScalastyle,
+      resolvers ++= Seq(
+        "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
+        "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
+      ),
+      publishTo <<= version { (v: String) =>
+        val nexus = "https://oss.sonatype.org/"
+        if (v.trim.endsWith("SNAPSHOT"))
+          Some("snapshots" at nexus + "content/repositories/snapshots")
+        else
+          Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+      },
+
+      /**
+        * publish
+        */
+      pgpReadOnly := false,
+      pomExtra in Global := {
+        <url>https://github.com/ImmobilienScout24/play-orientdb-client</url>
+          <licenses>
+            <license>
+              <name>Apache 2</name>
+              <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+            </license>
+          </licenses>
+          <scm>
+            <connection>scm:git:github.com/ImmobilienScout24/play-orientdb-client</connection>
+            <developerConnection>scm:git:git@github.com:ImmobilienScout24/play-orientdb-client</developerConnection>
+            <url>github.com/ImmobilienScout24/play-orientdb-client</url>
+          </scm>
+          <developers>
+            <developer>
+              <id>is24-phaun</id>
+              <name>Patrick Haun</name>
+              <url>http://github.com/bomgar</url>
+            </developer>
+          </developers>
+      },
+      releaseProcess := Seq[ReleaseStep](
+        checkSnapshotDependencies,
+        inquireVersions,
+        runTest,
+        setReleaseVersion,
+        commitReleaseVersion,
+        tagRelease,
+        releaseStepCommand("publishSigned"),
+        releaseStepCommand("sonatypeRelease"),
+        setNextVersion,
+        commitNextVersion,
+        pushChanges
+      )
 
 
-  ).enablePlugins(play.sbt.PlayScala)
+    ).enablePlugins(play.sbt.PlayScala)
 
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -8,4 +8,8 @@ addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.5")
 
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.4.6")
 
-addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8.5")
+addSbtPlugin("com.github.gseitz" % "sbt-release" % "1.0.0")
+
+addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "1.1")
+
+addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.0.0")
